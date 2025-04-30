5456e15ef827e153f34355d38f4635b928720646
Burak Kaygusuz <burakkaygusuz@outlook.com>
2025-04-30 10:32:35 +0300

    feat(service): enhance WebElementService with null safety

--- src/main/java/io/github/burakkaygusuz/services/WebElementService.java
+++ src/main/java/io/github/burakkaygusuz/services/WebElementService.java
@@ -1,24 +1,28 @@
 package io.github.burakkaygusuz.services;
 
 import io.github.burakkaygusuz.synchronizations.CustomConditions;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.jspecify.annotations.NullMarked;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Service class for working with WebElements with built-in wait conditions.
  * Implements SearchContext to provide enhanced element finding capabilities.
  */
+@NullMarked
 public class WebElementService implements SearchContext {
 
   private final WebDriverWait wait;
 
-  public WebElementService(WebDriverWait wait) {
+  public WebElementService(@NonNull WebDriverWait wait) {
     this.wait = wait;
   }
 
@@ -29,7 +33,8 @@ public class WebElementService implements SearchContext {
    * @return a list of matching WebElements
    */
   @Override
-  public List<WebElement> findElements(By by) {
+  @NonNull
+  public List<WebElement> findElements(@NonNull By by) {
     return wait.until(webDriver -> ExpectedConditions.presenceOfAllElementsLocatedBy(by).apply(webDriver));
   }
 
@@ -38,18 +43,17 @@ public class WebElementService implements SearchContext {
    *
    * @param by the locator to find the element
    * @return the matching WebElement
-   * @throws TimeoutException       if the element is not
-   *                                found within the timeout
-   *                                period
-   * @throws NoSuchElementException if the element cannot be
-   *                                located
+   * @throws TimeoutException if the element is not
+   *                          found within the timeout
+   *                          period
    */
   @Override
-  public WebElement findElement(By by) {
+  @NonNull
+  public WebElement findElement(@NonNull By by) {
     try {
       return wait.until(webDriver -> ExpectedConditions.presenceOfElementLocated(by).apply(webDriver));
     } catch (Exception e) {
-      throw new NoSuchElementException("Element not found with locator: " + by.toString(), e);
+      throw new TimeoutException("Element not found with locator: " + by, e);
     }
   }
 
@@ -59,7 +63,8 @@ public class WebElementService implements SearchContext {
    * @param by the locator to find the button
    * @return the clickable button WebElement
    */
-  public WebElement findButton(By by) {
+  @NonNull
+  public WebElement findButton(@NonNull By by) {
     WebElement webElement = this.findElement(by);
     return wait.until(webDriver -> ExpectedConditions.elementToBeClickable(webElement).apply(webDriver));
   }
@@ -70,7 +75,8 @@ public class WebElementService implements SearchContext {
    * @param by the locator to find the input
    * @return the typeable input WebElement
    */
-  public WebElement findInput(By by) {
+  @NonNull
+  public WebElement findInput(@NonNull By by) {
     WebElement webElement = this.findElement(by);
     return wait.until(webDriver -> CustomConditions.elementToBeTypeable(webElement).apply(webDriver));
   }
@@ -82,8 +88,9 @@ public class WebElementService implements SearchContext {
    * @param property the DOM property name to retrieve
    * @return the value of the specified DOM property
    */
-  public String findDomProperty(By by, String property) {
+  @NonNull
+  public String findDomProperty(@NonNull By by, @NonNull String property) {
     WebElement webElement = this.findElement(by);
-    return webElement.getDomProperty(property);
+    return Objects.requireNonNull(webElement.getDomProperty(property));
   }
 }
